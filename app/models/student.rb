09e0d995e837c2c5f8ff500aa614458b7949306b
Chris Franklin <cf73021@gmail.com>
2018-11-21 17:40:29 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,5 @@
 class Student < ActiveRecord::Base
   def self.to_s
-    @first_name = self.first_name
-    @last_name = self.last_name
-    @name_string = @first_name + " " + @last_name
-    @name_string
+    "#{first_name} #{last_name}"
   end
 end
